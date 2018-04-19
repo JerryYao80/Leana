QuantConnect/Lean

Adding new VS backtest ToolWindow that will allow the user to monitor and perform different actions over past and ongoing backtests.

Common/API/Backtest.cs
@@ -13,6 +13,7 @@
  * limitations under the License.
 */
 
+using System;
 using System.Collections.Generic;
 using Newtonsoft.Json;
 using QuantConnect.Packets;
@@ -71,6 +72,12 @@ namespace QuantConnect.Api
         /// </summary>
         [JsonProperty(PropertyName = "stacktrace")]
         public string StackTrace;
+
+        /// <summary>
+        /// Backtest creation date and time
+        /// </summary>
+        [JsonProperty(PropertyName = "creationDateTime")]
+        public DateTime CreationDateTime;
     }
 
     /// <summary>

JerryYao80/Leana

Common/API/Backtest.cs
@@ -13,6 +13,7 @@
  * limitations under the License.
 */
 
+using System;
 using System.Collections.Generic;
 using Newtonsoft.Json;
 using QuantConnect.Packets;
@@ -71,6 +72,12 @@ namespace QuantConnect.Api
         /// </summary>
         [JsonProperty(PropertyName = "stacktrace")]
         public string StackTrace;
+
+        /// <summary>
+        /// Backtest creation date and time
+        /// </summary>
+        [JsonProperty(PropertyName = "created")]
+        public DateTime Created;
     }
 
     /// <summary>