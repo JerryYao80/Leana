Madprankster/Lean

Remove disposed security data from TimeSlice
If we pull data and on the same time step that security gets removed,
we can still get that data in OnData(Slice) even though it was removed.
This change filters out removed securities by tracking a reference to
the subscription's disposed flag. Another change was made to wait until
the end of the time step to dispose of subscriptions.

Add IDataFeed.GetSubscription(SubscriptionDataConfig)

Engine/DataFeeds/DataFeedPacket.cs
@@ -1,11 +1,11 @@
 /*
  * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
  * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,6 +17,7 @@
 using System.Collections.Generic;
 using QuantConnect.Data;
 using QuantConnect.Securities;
+using QuantConnect.Util;
 
 namespace QuantConnect.Lean.Engine.DataFeeds
 {
@@ -25,7 +26,7 @@ namespace QuantConnect.Lean.Engine.DataFeeds
     /// </summary>
     public class DataFeedPacket
     {
-        private readonly List<BaseData> _data;
+        private readonly IReadOnlyRef<bool> _isDisposed;
 
         /// <summary>
         /// The security
@@ -46,29 +47,27 @@ namespace QuantConnect.Lean.Engine.DataFeeds
         /// <summary>
         /// Gets the number of data points held within this packet
         /// </summary>
-        public int Count
-        {
-            get { return _data.Count; }
-        }
+        public int Count => Data.Count;
 
         /// <summary>
         /// The data for the security
         /// </summary>
-        public List<BaseData> Data
-        {
-            get { return _data; }
-        }
+        public List<BaseData> Data { get; }
+
+        /// <summary>
+        /// Gets whether or not this packet should be disposed due to the subscription being removed
+        /// </summary>
+        public bool IsDisposed => _isDisposed.Value;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="DataFeedPacket"/> class
         /// </summary>
         /// <param name="security">The security whose data is held in this packet</param>
         /// <param name="configuration">The subscription configuration that produced this data</param>
-        public DataFeedPacket(Security security, SubscriptionDataConfig configuration)
+        /// <param name="isDisposed">Reference to whether or not the subscription has since been disposed, defaults to false</param>
+        public DataFeedPacket(Security security, SubscriptionDataConfig configuration, IReadOnlyRef<bool> isDisposed = null)
+            : this(security, configuration, new List<BaseData>(), isDisposed)
         {
-            Security = security;
-            Configuration = configuration;
-            _data = new List<BaseData>();
         }
 
         /// <summary>
@@ -78,11 +77,13 @@ namespace QuantConnect.Lean.Engine.DataFeeds
         /// <param name="configuration">The subscription configuration that produced this data</param>
         /// <param name="data">The data to add to this packet. The list reference is reused
         /// internally and NOT copied.</param>
-        public DataFeedPacket(Security security, SubscriptionDataConfig configuration, List<BaseData> data)
+        /// <param name="isDisposed">Reference to whether or not the subscription has since been disposed, defaults to false</param>
+        public DataFeedPacket(Security security, SubscriptionDataConfig configuration, List<BaseData> data, IReadOnlyRef<bool> isDisposed = null)
         {
             Security = security;
             Configuration = configuration;
-            _data = data;
+            Data = data;
+            _isDisposed = isDisposed ?? Ref.Create(false);
         }
 
         /// <summary>
@@ -91,7 +92,7 @@ namespace QuantConnect.Lean.Engine.DataFeeds
         /// <param name="data">The data to be added to this packet</param>
         public void Add(BaseData data)
         {
-            _data.Add(data);
+            Data.Add(data);
         }
     }
 }

JerryYao80/Leana

Engine/DataFeeds/DataFeedPacket.cs
@@ -1,11 +1,11 @@
 /*
  * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
  * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,6 +17,7 @@
 using System.Collections.Generic;
 using QuantConnect.Data;
 using QuantConnect.Securities;
+using QuantConnect.Util;
 
 namespace QuantConnect.Lean.Engine.DataFeeds
 {
@@ -25,7 +26,7 @@ namespace QuantConnect.Lean.Engine.DataFeeds
     /// </summary>
     public class DataFeedPacket
     {
-        private readonly List<BaseData> _data;
+        private readonly IReadOnlyRef<bool> _isRemoved;
 
         /// <summary>
         /// The security
@@ -46,29 +47,27 @@ namespace QuantConnect.Lean.Engine.DataFeeds
         /// <summary>
         /// Gets the number of data points held within this packet
         /// </summary>
-        public int Count
-        {
-            get { return _data.Count; }
-        }
+        public int Count => Data.Count;
 
         /// <summary>
         /// The data for the security
         /// </summary>
-        public List<BaseData> Data
-        {
-            get { return _data; }
-        }
+        public List<BaseData> Data { get; }
+
+        /// <summary>
+        /// Gets whether or not this packet should be filtered out due to the subscription being removed
+        /// </summary>
+        public bool IsSubscriptionRemoved => _isRemoved.Value;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="DataFeedPacket"/> class
         /// </summary>
         /// <param name="security">The security whose data is held in this packet</param>
         /// <param name="configuration">The subscription configuration that produced this data</param>
-        public DataFeedPacket(Security security, SubscriptionDataConfig configuration)
+        /// <param name="isSubscriptionRemoved">Reference to whether or not the subscription has since been removed, defaults to false</param>
+        public DataFeedPacket(Security security, SubscriptionDataConfig configuration, IReadOnlyRef<bool> isSubscriptionRemoved = null)
+            : this(security, configuration, new List<BaseData>(), isSubscriptionRemoved)
         {
-            Security = security;
-            Configuration = configuration;
-            _data = new List<BaseData>();
         }
 
         /// <summary>
@@ -78,11 +77,13 @@ namespace QuantConnect.Lean.Engine.DataFeeds
         /// <param name="configuration">The subscription configuration that produced this data</param>
         /// <param name="data">The data to add to this packet. The list reference is reused
         /// internally and NOT copied.</param>
-        public DataFeedPacket(Security security, SubscriptionDataConfig configuration, List<BaseData> data)
+        /// <param name="isSubscriptionRemoved">Reference to whether or not the subscription has since been removed, defaults to false</param>
+        public DataFeedPacket(Security security, SubscriptionDataConfig configuration, List<BaseData> data, IReadOnlyRef<bool> isSubscriptionRemoved = null)
         {
             Security = security;
             Configuration = configuration;
-            _data = data;
+            Data = data;
+            _isRemoved = isSubscriptionRemoved ?? Ref.Create(false);
         }
 
         /// <summary>
@@ -91,7 +92,7 @@ namespace QuantConnect.Lean.Engine.DataFeeds
         /// <param name="data">The data to be added to this packet</param>
         public void Add(BaseData data)
         {
-            _data.Add(data);
+            Data.Add(data);
         }
     }
 }